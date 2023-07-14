c78fe1049c2ac3b571565540380d30e255aa19cc
aaron <20100312@mail.wit.ie>
2023-07-14 11:21:12 +0100

    Implemented comprehensive Swagger documentation for Books and ReadingList API endpoints, outlining their data structures, request bodies, and response schemas.

--- src/app.py
+++ src/app.py
@@ -3,11 +3,27 @@ from flask_sqlalchemy import SQLAlchemy
 from src.helpers import *
 from src.config import config
 import os
+from flask_swagger_ui import get_swaggerui_blueprint
 
-app = Flask("book_status_api")
+app = Flask("book_status_api", static_url_path='/static')
 # Load config to app
 app.config.from_pyfile("src/config/config.py")
 app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_URI
+
+# Swagger UI setup
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.yml'  # Path to your YAML file
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be served at '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Book Status Service API"
+    },
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 db = SQLAlchemy(app)
 
