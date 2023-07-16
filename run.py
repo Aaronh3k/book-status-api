37576e546f3304f6c10a24fd2f626f550f90ba2a
aaron <20100312@mail.wit.ie>
2023-07-16 13:17:47 +0100

    Configured Elastic Beanstalk deployment and DB migration

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
 from src.app import app
+from alembic.config import Config
+from alembic import command
 
 if __name__ == "__main__":
+  if app.config['APP_ENVIRONMENT'] != "test":
+    # Run alembic migrations before starting the server
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
   if app.config['APP_ENVIRONMENT'] == "dev":
     app.run(host='127.0.0.1', port=5000, debug=True)
   else:
