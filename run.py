444c05ad67d4bedf697e20db2caca37258fa3018
aaron <20100312@mail.wit.ie>
2023-07-16 16:01:50 +0100

    Test CI/CD

--- run.py
+++ run.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
   if app.config['APP_ENVIRONMENT'] == "dev":
     app.run(host='127.0.0.1', port=5000, debug=True)
   else:
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
